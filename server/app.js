2aaae7a55609be8dce7a27118dfd604ac8c85d6b
Mukesh Suthar <mukeshsuthar336@gmail.com>
2020-11-28 16:55:20 +0000

    Add cors

--- server/app.js
+++ server/app.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const schema = require("./schema/schema");
 
 const app = express();
 const PORT = 9090;
 
+// Allow cross origin requests
+app.use(cors());
+
 mongoose.connect("mongodb://localhost:27017/booksDb", { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.connection.once('open',()=>{
+mongoose.connection.once('open', () => {
     console.log("connected to database")
 })
 
